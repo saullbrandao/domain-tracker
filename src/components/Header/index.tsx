b035dfc112ae09e54f8cc3b5ebb9d881cafdf73b
Saull Brandão <saull@outlook.com>
2021-07-27 09:52:23 -0300

    feat: smaller font

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <header>
-      <h1 className="text-white text-4xl font-bold mt-4">IP Address Tracker</h1>
+      <h1 className="text-white text-2xl font-bold mt-6">IP Address Tracker</h1>
     </header>
   )
 }

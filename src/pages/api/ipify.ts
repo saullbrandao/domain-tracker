a0f03856d86b8df66ffbd7a31bea2e7a0e411821
Saull Brandão <saull@outlook.com>
2021-07-28 08:10:43 -0300

    feat: bff to get data from the ipify api

---
+++ src/pages/api/ipify.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { api } from 'services/api'
+
+type Location = {
+  region: string
+  country: string
+  postalCode: string
+  timezone: string
+  lat: number
+  lng: number
+}
+
+type Data = {
+  ip: string
+  isp: string
+  location: Location
+}
+
+type Error = {
+  message: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | Error>,
+) {
+  try {
+    const { data } = await api.get('', {
+      params: {
+        apiKey: process.env.NEXT_API_KEY,
+        domain: req.query.domain,
+      },
+    })
+    const newData = {
+      ip: data.ip,
+      isp: data.isp,
+      location: {
+        region: data.location.region,
+        country: data.location.country,
+        postalCode: data.location.postalCode,
+        timezone: data.location.timezone,
+        lat: data.location.lat,
+        lng: data.location.lng,
+      },
+    }
+
+    res.json(newData)
+  } catch (err) {
+    res.status(500).json({ message: 'error' })
+  }
+}

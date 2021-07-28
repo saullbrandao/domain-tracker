b169f741961ff21ef0189349f202d5c80656d475
Saull Brandão <saull@outlook.com>
2021-07-28 08:12:41 -0300

    fix: change button type to submit

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,10 @@ import arrow from 'public/icon-arrow.svg'
 
 export function Button() {
   return (
-    <button className="bg-black px-5 py-3 rounded-r-xl hover:bg-gray-800">
+    <button
+      className="bg-black px-5 py-3 rounded-r-xl hover:bg-gray-800"
+      type="submit"
+    >
       <Image src={arrow} alt="search button" />
     </button>
   )

3ac48cd91994a15a16e36608966c04632f969120
Saull Brandão <saull@outlook.com>
2021-07-28 08:13:38 -0300

    feat: reduce the gap between components

--- src/templates/HomePage/index.tsx
+++ src/templates/HomePage/index.tsx
@@ -5,7 +5,7 @@ import { Header } from 'components/Header'
 export function HomePage() {
   return (
     <main>
-      <section className="flex flex-col gap-8 items-center bg-pattern-bg h-72 relative">
+      <section className="flex flex-col gap-6 items-center bg-pattern-bg h-72 relative">
         <Header />
         <Form />
         <DetailsSection />

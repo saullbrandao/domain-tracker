d4a6be0121d3472ea824127fc85cfe5b16008ecf
Saull Brandão <saull@outlook.com>
2021-07-28 08:34:46 -0300

    feat: user can type a domain and click on the button to search info about it

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,25 @@
 import { Button } from 'components/Button'
 import { Input } from 'components/Input'
+import { FormEvent, SyntheticEvent, useState } from 'react'
+import { useTracker } from 'src/hooks/useTracker'
 
 export function Form() {
+  const { handleDomainChange } = useTracker()
+  const [searchTerm, setSearchTerm] = useState('')
+
+  function handleSearch(event: FormEvent<HTMLInputElement>) {
+    setSearchTerm(event.currentTarget.value)
+  }
+
+  function handleSubmit(event: SyntheticEvent) {
+    event.preventDefault()
+    searchTerm && handleDomainChange(searchTerm)
+    setSearchTerm('')
+  }
+
   return (
-    <form className="flex justify-between w-5/6">
-      <Input />
+    <form className="flex justify-between w-5/6" onSubmit={handleSubmit}>
+      <Input searchTerm={searchTerm} handleSearch={handleSearch} />
       <Button />
     </form>
   )
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,18 @@
-export function Input() {
+import { FormEvent } from 'react'
+
+type InputProps = {
+  searchTerm: string
+  handleSearch: (event: FormEvent<HTMLInputElement>) => void
+}
+
+export function Input({ searchTerm, handleSearch }: InputProps) {
   return (
     <input
       className="rounded-l-xl pl-5 focus:outline-none focus:ring-2 focus:ring-black focus:ring-inset flex-1 "
       type="text"
-      placeholder="ip or domain"
+      placeholder="type the domain"
+      value={searchTerm}
+      onChange={handleSearch}
     />
   )
 }

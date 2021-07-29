8f279edfa8764f2ee0d4c5db4bd3d0b4ade3521b
Saull Brandão <saull@outlook.com>
2021-07-29 09:41:23 -0300

    feat: disable the button when the api is being fetched

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import arrow from 'public/icon-arrow.svg'
+import { Props } from './types'
 
-export function Button() {
+export function Button({ disabled, children }: Props) {
   return (
     <button
-      className="bg-black px-5 min-w-max h-full py-3 rounded-r-xl hover:bg-gray-dark "
+      className={`bg-black px-5 min-w-max h-full py-3 rounded-r-xl hover:bg-gray-dark ${
+        disabled && 'cursor-not-allowed'
+      }`}
       type="submit"
+      disabled={disabled}
     >
+      {/* TODO: show a loading spinner when fetching the api */}
       <Image src={arrow} alt="search button" />
     </button>
   )

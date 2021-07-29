34b97264859d03bc29472b7904ad1ad02b5cf32e
Saull Brandão <saull@outlook.com>
2021-07-29 09:59:39 -0300

    fix: remove the children prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import arrow from 'public/icon-arrow.svg'
 import { Props } from './types'
 
-export function Button({ disabled, children }: Props) {
+export function Button({ disabled }: Props) {
   return (
     <button
       className={`bg-black px-5 min-w-max h-full py-3 rounded-r-xl hover:bg-gray-dark ${
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,6 +1,3 @@
-import { ReactNode } from 'react'
-
 export type Props = {
-  disabled: boolean
-  children?: ReactNode
+  disabled?: boolean
 }

6b6c59fbd1ae63f0f6d56f3a2a3c341675fcf216
Saull Brandão <saull@outlook.com>
2021-07-29 11:29:49 -0300

    fix: change from h3 to h2 to properly order the headings

--- src/components/DomainInfo/index.tsx
+++ src/components/DomainInfo/index.tsx
@@ -3,9 +3,9 @@ import { DomainInfoProps } from './types'
 export const DomainInfo = ({ info, title }: DomainInfoProps) => {
   return (
     <article>
-      <h3 className="uppercase text-gray-light font-extrabold text-xs">
+      <h2 className="uppercase text-gray-light font-extrabold text-xs">
         {title}
-      </h3>
+      </h2>
       <p className="text-lg text-gray-dark">{info}</p>
     </article>
   )

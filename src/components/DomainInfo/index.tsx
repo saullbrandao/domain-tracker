c7026a11c50bfc01475ffee8c0f6b3626eff47ea
Saull Brandão <saull@outlook.com>
2021-07-29 09:23:16 -0300

    feat: change the text colors

--- src/components/DomainInfo/index.tsx
+++ src/components/DomainInfo/index.tsx
@@ -2,11 +2,11 @@ import { DomainInfoProps } from './types'
 
 export const DomainInfo = ({ info, title }: DomainInfoProps) => {
   return (
-    <article className="flex-1">
-      <h3 className="uppercase text-gray-400 font-extrabold text-xs">
+    <article>
+      <h3 className="uppercase text-gray-light font-extrabold text-xs">
         {title}
       </h3>
-      <p className="text-lg">{info}</p>
+      <p className="text-lg text-gray-dark">{info}</p>
     </article>
   )
 }

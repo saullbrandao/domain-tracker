aa21e865e967fe253f06a904c9dbe285cf07c3d3
Saull Brandão <saull@outlook.com>
2021-10-09 07:49:29 -0300

    feat: improve contrast between fg and bg

--- src/components/DomainInfo/index.tsx
+++ src/components/DomainInfo/index.tsx
@@ -3,7 +3,7 @@ import { DomainInfoProps } from './types'
 export const DomainInfo = ({ info, title }: DomainInfoProps) => {
   return (
     <article>
-      <h2 className="uppercase text-gray-light font-extrabold text-xs">
+      <h2 className="uppercase text-gray-dark font-extrabold text-xs">
         {title}
       </h2>
       <p className="text-lg text-gray-dark">{info}</p>
